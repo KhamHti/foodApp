fe4d7fdf3db53d819d4f715998934b80e79ce146
KhamHti <90329989+KhamHti@users.noreply.github.com>
2023-07-26 01:46:49 +0630

    update navigation

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import SearchScreen from "./src/screens/SearchScreen";
+import ResultsShowScreen from "./src/screens/ResultsShowScreen";
 
 const App = () => {
   const Stack = createNativeStackNavigator();
@@ -10,6 +11,7 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="SearchScreen" component={SearchScreen} options={{headerShown: false}}/>
+        <Stack.Screen name="ResultsShow" component={ResultsShowScreen} options={{headerShown: false}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
